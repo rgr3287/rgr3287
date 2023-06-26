7d886489f76ef4d325e9a580728f7f0c887a14f8
hyeongeun gwak <hggwak@grooo.co.jp>
2023-06-26 17:45:54 +0900

    creat.main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import feedparser, time
+
+URL = "https://rgr3287.tistory.com/rss"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST = 5
+
+markdown_text = """
+## ✅ Latest Blog Post
+
+"""  # list of blog posts will be appended here
+
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
